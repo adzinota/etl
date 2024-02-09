e12bcca585b91a9ff60bebe607e9834e8557cc01
Глазунова Яна <107113641+adzinota@users.noreply.github.com>
2024-02-09 07:47:09 +0300

    .

--- task1_v2/etl_dags.py
+++ task1_v2/etl_dags.py
@@ -36,26 +36,15 @@ def df_transform(name):
                 df[column] = pd.to_datetime(df[column], dayfirst=True)
             except:
                 df[column] = pd.to_datetime(df[column])
-
+    
     return df
 
-# Выбор трансформации в зависимости от имени файла
-def select_transform(name_file):
-    tasks_for_select = {'ft_balance_f': transform_0,
-                        'ft_posting_f': transform_1,
-                        'md_account_d': transform_0,
-                        'md_currency_d': transform_0,
-                        'md_exchange_rate_d': transform_2,
-                        'md_ledger_account_s': transform_0}
-    return tasks_for_select[name_file]
-
 # Запрос для логов (начало и конец процесса) 
-def query_logs_tables(type_log, tname):
+def query_logs_tables(action_type, table_name):
 
     return f'''
         INSERT INTO logs.logs_etl
-        VALUES ('{tname}', '{datetime.now()}',
-        '{type_log}')
+        VALUES ('{table_name}', '{datetime.now()}', '{action_type}')
         '''
 
 # -------------------------------------------------------------------------------------------
@@ -64,58 +53,40 @@ def query_logs_tables(type_log, tname):
 
 # Извлечение исходных данных из файлов и сохранение во временную папку
 @task(task_id='extract')
-def extract(name_file_csv):
+def extract(name):
 
-    path = f'{IMPORT_PATH}/{name_file_csv}.csv'
+    path = f'{IMPORT_PATH}/{name}.csv'
     
     try:
         df = pd.read_csv(path, sep=';', keep_default_na=False)
     except:
         df = pd.read_csv(path, sep=';', keep_default_na=False, encoding='CP866')
     
-    path = f'{EXPORT_PATH}/{name_file_csv}.csv'
+    path = f'{EXPORT_PATH}/{name}.csv'
     df.to_csv(path, index=False, encoding='UTF-8')
 
-# Основная трансформация для тех файлов, где не требуется дополнительная обработка
-@task(task_id='transform_0')
-def transform_0(name_df):
+# Трансформация файлов и сохранение во временную папку
+@task(task_id='transform')
+def transform(name):
 
-    pd_df = df_transform(name_df)
+    df = df_transform(name)
 
-    path = f'{EXPORT_PATH}/{name_df}.csv'
-    pd_df.to_csv(path, index=False, encoding='UTF-8')
+    if name == 'ft_posting_f':
+        columns = ['OPER_DATE', 'CREDIT_ACCOUNT_RK', 'DEBET_ACCOUNT_RK']
+        df = df.groupby(columns, as_index=False).sum()
+    elif name == 'md_exchange_rate_d':
+        df = df.drop_duplicates()
 
-# Трансформация для ft_posting_f.csv с группировкой по полям
-@task(task_id='transform_1')
-def transform_1(name_df):
-
-    pd_df = df_transform(name_df)
-
-    columns = ['OPER_DATE', 'CREDIT_ACCOUNT_RK', 'DEBET_ACCOUNT_RK']
-
-    pd_df = pd_df.groupby(columns, as_index=False).sum()
-
-    path = f'{EXPORT_PATH}/{name_df}.csv'
-    pd_df.to_csv(path, index=False, encoding='UTF-8')
-
-# Трансформация для md_exchange_rate_d.csv с удалением дубликатов
-@task(task_id='transform_2')
-def transform_2(name_df):
-
-    pd_df = df_transform(name_df)
-
-    pd_df = pd_df.drop_duplicates()
-
-    path = f'{EXPORT_PATH}/{name_df}.csv'
-    pd_df.to_csv(path, index=False, encoding='UTF-8')
+    path = f'{EXPORT_PATH}/{name}.csv'
+    df.to_csv(path, index=False, encoding='UTF-8')
 
 # Загрузка обработанных датафреймов в базу данных
 @task(task_id='load')
-def load(table_name):
+def load(name):
     metadata_obj = MetaData(schema = 'ds')
-    table = Table(table_name, metadata_obj, autoload_with=CONNECTION)
+    table = Table(name, metadata_obj, autoload_with=CONNECTION)
     
-    df = pd.read_csv(f'{EXPORT_PATH}/{table_name}.csv', keep_default_na=False)
+    df = pd.read_csv(f'{EXPORT_PATH}/{name}.csv', keep_default_na=False)
 
     insert_statement = insert(table).values(df.values.tolist())
     upsert_statement = insert_statement.on_conflict_do_update(constraint=table.primary_key, set_=dict(insert_statement.excluded))
@@ -130,26 +101,26 @@ with open('/home/joann/airflow/dags/dag_config.yaml', 'r') as file:
 
 for dag_id, config in dags.items():
 
-    with DAG(f'{config["dag_id"]}',
+    with DAG(f'{config["dag_name"]}',
          start_date=datetime(2024,1,1),
          schedule_interval=None,
-         catchup=False) as dag_id:
+         catchup=False) as dag_name:
 
         # Таск sleep
         sleep_5s = BashOperator(task_id='sleep_5s',
-                            dag=dag_id,
+                            dag=dag_name,
                             bash_command='sleep 5s')
         
         # Таск start
         @task(task_id=f'start')
-        def start(table_for_query, **kwargs):
-            CONNECTION.execute(query_logs_tables('start', table_for_query))
+        def start(name, **kwargs):
+            CONNECTION.execute(query_logs_tables('start', name))
 
         # Таск finish
         @task(task_id=f'finish')
-        def finish(table_for_query, **kwargs):
-            CONNECTION.execute(query_logs_tables('finish', table_for_query))
+        def finish(name, **kwargs):
+            CONNECTION.execute(query_logs_tables('finish', name))
             
-        filename = config['filename']
+        name = config['file_name']
 
-        start(filename) >> sleep_5s >> extract(filename) >> select_transform(filename)(filename) >> load(filename) >> finish(filename)
+        start(name) >> sleep_5s >> extract(name) >> transform(name) >> load(name) >> finish(name)
